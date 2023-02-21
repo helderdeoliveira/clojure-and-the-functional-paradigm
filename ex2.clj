11b7830c2798386d7037b4b25b5331f7746e8acc
Helder <helder.deoliveira@codurance.com>
2023-02-21 08:53:54 +0100

    fizzbuzz with map

--- ex2.clj
+++ ex2.clj
@@ -46,4 +46,27 @@
 (println (contains? x :name))
 (println (contains? x :name))
 
+(println "----------------------- A NOT-SO-READABLE FIZZBUZZ -----------------------")
+(defn fizzbuzz [ns]
+  (map
+       (fn [n] (cond
+                 (= 0 (mod n 15)) "FizzBuzz"
+                 (= 0 (mod n 3)) "Fizz"
+                 (= 0 (mod n 5)) "Buzz"
+                 :else n
+                 )) ns))
+(println (fizzbuzz '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))
 
+(println "----------------------- A MORE READABLE FIZZBUZZ -----------------------")
+(defn innerfizzbuzz [n]
+  (cond
+    (= 0 (mod n 15)) "FizzBuzz"
+    (= 0 (mod n 3)) "Fizz"
+    (= 0 (mod n 5)) "Buzz"
+    :else n
+    ))
+
+(defn fizzbuzz [ns]
+  (map innerfizzbuzz ns))
+
+(println (fizzbuzz '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))
