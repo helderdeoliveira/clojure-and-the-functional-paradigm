f56f27dbe818deeb65dc00d8ed7d9004188e21ad
Helder <helder.deoliveira@codurance.com>
2023-02-21 08:44:26 +0100

    some important collection operations

--- ex2.clj
+++ ex2.clj
@@ -10,7 +10,40 @@
 (println (fizzbuzz '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)))
 
 
-(println "----------------------- MAP -----------------------")
 (def x (range 1 10))
+(def y [1 2 3 4 5])
+(def z '(6 7 8 9))
+
+(println "----------------------- MAP -----------------------")
 (println x)
 (println (map inc x))
+
+(println "----------------------- REDUCE -----------------------")
+(println (reduce + x))
+
+(println "----------------------- POP -----------------------")
+(println (pop y))
+(println (pop z))
+
+(println "----------------------- PEEK -----------------------")
+(println (peek y))
+(println (peek z))
+
+(println "----------------------- CONVERT COLLECTIONS -----------------------")
+(println (map inc x))
+(println (into [] (map inc x)))
+
+(println "----------------------- GROUP BY -----------------------")
+(println (group-by even? x))
+
+(println "----------------------- CONTAINS -----------------------")
+(println (contains? y 2))
+
+(println "----------------------- RECORD (kinda) -----------------------")
+(def x {:name "Helder", :surname "De Oliveira"})
+(println x)
+(println (class x))
+(println (contains? x :name))
+(println (contains? x :name))
+
+
